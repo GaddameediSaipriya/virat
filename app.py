54032ba53deaf74d2a92139ccb75b24b1e40070b
GaddameediSaipriya <113419037+GaddameediSaipriya@users.noreply.github.com>
2022-09-13 17:09:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,27 @@ input = st.text_input('enter the score and Batting order')
 output = model.predict([input])
 if st.button('predict'):
   st.title(output[0])         
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
