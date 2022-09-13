9906288141849cd6b027225ee0139c2533e8307c
GaddameediSaipriya <113419037+GaddameediSaipriya@users.noreply.github.com>
2022-09-13 16:49:18 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+%%writefile app.py
+import streamlit as streamlitimport joblib
+mpdel = joblib.load('virat')
+st.title('Virat Centuries')
+input = st.text_input('enter the score and Batting order')
+output = model.predict([input])
+if st.button('predict'):
+  st.title(output[0])
