ecffb765e240a5e28816a53c845fab5a07638128
GaddameediSaipriya <113419037+GaddameediSaipriya@users.noreply.github.com>
2022-09-13 17:13:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 %%writefile app.py
-import streamlit as streamlitimport joblib
-mpdel = joblib.load('virat')
+import streamlit as streamlit
+import joblib
+model = joblib.load('virat')
 st.title('Virat Centuries')
 input = st.text_input('enter the score and Batting order')
 output = model.predict([input])
